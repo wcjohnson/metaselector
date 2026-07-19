5fc783b7fe308d4860e409be6f86466c84997888
William C. Johnson <wcjohnson@oigroup.net>
2026-07-19 02:01:14 -0700

    feat: ingredients-to-products impl

--- control/metadata.lua
+++ control/metadata.lua
@@ -1,4 +1,5 @@
 local tlib = require("lib.core.table")
+local siglib = require("lib.core.signal")
 local strace = require("lib.core.strace")
 
 local ipairs = ipairs
@@ -21,6 +22,34 @@ end
 ---@class Metaselector.MachineMetadata
 ---@field public recipes table<string, LuaRecipePrototype> Recipes that can be crafted on this machine on this surface
 ---@field public recipes_by_product table<string, LuaRecipePrototype> Recipe that produces this product. "last" recipe returned by Factorio takes precedence.
+---@field public variant_count integer
+---@field public variants Metaselector.RecipeVariant[]
+---@field public variants_by_ingredient_key table<SignalKey, integer[]>
+
+---@class Metaselector.RecipeVariant
+---@field public recipe LuaRecipePrototype
+---@field public quality string
+---@field public product SignalID
+---@field public required_count integer
+---@field public required_keys SignalKey[]
+---@field public required_amounts integer[]
+
+---@type string[]?
+local _quality_names
+
+---@return string[]
+local function get_quality_names()
+	if _quality_names then return _quality_names end
+	---@type string[]
+	local quality_names = {}
+	for quality_name in pairs(prototypes.quality or EMPTY) do
+		quality_names[#quality_names + 1] = quality_name
+	end
+	if #quality_names == 0 then quality_names[1] = "normal" end
+	table.sort(quality_names)
+	_quality_names = quality_names
+	return quality_names
+end
 
 ---@type table<string, table<int, Metaselector.MachineMetadata>>
 local _machine_metadata = {}
@@ -42,9 +71,18 @@ function lib.get_machine_metadata(machine_name, surface)
 
 	local recipes = {}
 	local recipes_by_product = {}
+	---@type Metaselector.RecipeVariant[]
+	local variants = {}
+	---@type table<SignalKey, integer[]>
+	local variants_by_ingredient_key = {}
 	---@type Metaselector.MachineMetadata
-	local metadata =
-		{ recipes = recipes, recipes_by_product = recipes_by_product }
+	local metadata = {
+		recipes = recipes,
+		recipes_by_product = recipes_by_product,
+		variant_count = 0,
+		variants = variants,
+		variants_by_ingredient_key = variants_by_ingredient_key,
+	}
 
 	local mproto = prototypes.entity[machine_name]
 	if not mproto then return end
@@ -55,13 +93,62 @@ function lib.get_machine_metadata(machine_name, surface)
 	end
 
 	local fr = prototypes.get_recipe_filtered(filters)
+	local quality_names = get_quality_names()
 	for name, recipe in pairs(fr) do
 		if not can_craft_here(surface, recipe) then goto continue end
 		recipes[name] = recipe
 		local main_product = recipe.main_product
 		if main_product then recipes_by_product[main_product.name] = recipe end
+
+		local ingredients = recipe.ingredients or EMPTY
+		if main_product and #ingredients > 0 then
+			local product_type = main_product.type or "item"
+			for _, quality_name in ipairs(quality_names) do
+				---@type SignalID
+				local product = {
+					type = product_type,
+					name = main_product.name,
+					quality = product_type == "item" and quality_name or "normal",
+				}
+
+				---@type SignalKey[]
+				local required_keys = {}
+				---@type integer[]
+				local required_amounts = {}
+				for _, ingredient in ipairs(ingredients) do
+					local ingredient_type = ingredient.type or "item"
+					required_keys[#required_keys + 1] = siglib.encode_signal_key(
+						ingredient.name,
+						ingredient_type,
+						ingredient_type == "item" and quality_name or "normal"
+					)
+					required_amounts[#required_amounts + 1] = math.ceil(ingredient.amount)
+				end
+
+				local variant_id = #variants + 1
+				variants[variant_id] = {
+					recipe = recipe,
+					quality = quality_name,
+					product = product,
+					required_count = #required_keys,
+					required_keys = required_keys,
+					required_amounts = required_amounts,
+				}
+				for i = 1, #required_keys do
+					local key = required_keys[i]
+					local recipe_ids = variants_by_ingredient_key[key]
+					if recipe_ids then
+						recipe_ids[#recipe_ids + 1] = variant_id
+					else
+						variants_by_ingredient_key[key] = { variant_id }
+					end
+				end
+			end
+		end
 		::continue::
 	end
+
+	metadata.variant_count = #variants
 
 	by_name[surface_index] = metadata
 	return metadata
--- control/modes/ingredients-to-products.lua
+++ control/modes/ingredients-to-products.lua
@@ -10,6 +10,10 @@ local get_machine_metadata = require("control.metadata").get_machine_metadata
 local VF = ultros.VFlow
 local get_tag = things_client.tags_v1.get_tag
 local EMPTY = tlib.EMPTY
+local signal_to_key = siglib.signal_to_key
+local signals_to_counts = siglib.signals_to_counts
+local ipairs = ipairs
+local pairs = pairs
 
 mode_lib.register_mode({
 	name = "ingredients-to-products",
@@ -18,7 +22,94 @@ mode_lib.register_mode({
 	script_inputs = true,
 	script_update =
 		---@param combinator Metaselector.Combinator
-		function(combinator) end,
+		function(combinator)
+			local profiler = helpers.create_profiler()
+			local id = combinator.id
+			local inputs = (combinator.inputs or EMPTY) --[[@as Signal[] ]]
+			local machine_sig = get_tag(id, "machine") --[[@as string?]]
+			local metadata =
+				get_machine_metadata(machine_sig, combinator.entity.surface)
+			if not metadata then
+				combinator:direct_write_outputs(EMPTY)
+				return
+			end
+			local variants_by_ingredient_key = metadata.variants_by_ingredient_key
+
+			---@type SignalCounts
+			local input_counts = signals_to_counts(inputs)
+
+			local prof_1 = helpers.create_profiler()
+			---@type table<integer, integer>
+			local candidate_hits = {}
+			local candidate_count = 0
+			for key in pairs(input_counts) do
+				local variant_ids = variants_by_ingredient_key[key]
+				if variant_ids then
+					for i = 1, #variant_ids do
+						local variant_id = variant_ids[i]
+						local hits = candidate_hits[variant_id]
+						if hits == nil then
+							candidate_hits[variant_id] = 1
+							candidate_count = candidate_count + 1
+						else
+							candidate_hits[variant_id] = hits + 1
+						end
+					end
+				end
+			end
+			prof_1.stop()
+			log({
+				"",
+				"ingredients-to-products: candidate_hits took ",
+				prof_1,
+				" for hits ",
+				candidate_count,
+			})
+
+			local prof_2 = helpers.create_profiler()
+			---@type table<SignalKey, boolean>
+			local seen_outputs = {}
+			---@type DeciderCombinatorOutput[]
+			local outputs = {}
+			for variant_id, hit_count in pairs(candidate_hits) do
+				local variant = metadata.variants[variant_id] --[[@as Metaselector.RecipeVariant]]
+				if hit_count == variant.required_count then
+					local can_make = true
+					for i = 1, variant.required_count do
+						local key = variant.required_keys[i]
+						if (input_counts[key] or 0) < variant.required_amounts[i] then
+							can_make = false
+							break
+						end
+					end
+
+					if can_make then
+						local product_key = signal_to_key(variant.product)
+						if not seen_outputs[product_key] then
+							seen_outputs[product_key] = true
+							outputs[#outputs + 1] = {
+								signal = variant.product,
+								copy_count_from_input = false,
+								constant = 1,
+							}
+						end
+					end
+				end
+			end
+			prof_2.stop()
+			log({
+				"",
+				"ingredients-to-products: output generation took ",
+				prof_2,
+				" for outputs ",
+				#outputs,
+			})
+
+			profiler.stop()
+			---@diagnostic disable-next-line: param-type-mismatch
+			log({ "", "ingredients-to-products took ", profiler })
+			combinator:direct_write_outputs(outputs)
+		end,
 })
 
 relm.define("Settings.IngredientsToProducts", function(props)
